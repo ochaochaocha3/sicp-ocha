70bfc21ce7808dc7b613a7988d0f10eb2ba6bf3b
ocha <ochaochaocha3@gmail.com>
2015-04-08 17:05:13 +0900

    1.22: 解き直し

--- ex-1-22.scm
+++ ex-1-22.scm
@@ -7,26 +7,23 @@
 (load "./ch-1.scm")
 
 (define (timed-prime-test n)
-  (start-prime-test n (runtime)))
+  (display n)
+  (start-prime-test n (runtime))
+  (newline))
 
 (define (start-prime-test n start-time)
   (if (prime? n)
     (report-prime n (- (runtime) start-time))))
 
 (define (report-prime n elapsed-time)
-  (print n "," elapsed-time))
+  (display " *** ")
+  (display elapsed-time))
 
 (define (search-for-primes start end)
-  (define (iter n)
-    (cond ((<= n end) (timed-prime-test n)
-                      (iter (+ n 2)))))
-  (if (even? start)
-    (iter (+ start 1))
-    (iter start)))
-
-(define (sp-test)
-  (define l
-    (map inexact->exact '(1e3 1e4 1e5 1e6)))
-  (define (sp-100 x)
-    (search-for-primes x (+ x 100)))
-  (for-each sp-100 l))
+  (cond ((> start end) 'finished)
+        ((< start 2) (search-for-primes 2 end))
+        (else (timed-prime-test start)
+              (search-for-primes
+                (+ start
+                   (if (even? start) 1 2))
+                end))))

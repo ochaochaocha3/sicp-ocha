9beec640005a6ea954cbfa47531e773d7a16f9bd
ocha <ochaochaocha3@gmail.com>
2015-04-12 16:45:41 +0900

    問題 1.23：微調整

--- ex-1-23.scm
+++ ex-1-23.scm
@@ -1,16 +1,20 @@
-(load "./ch-1.scm")
-(load "./ex-1-22.scm")
+(add-load-path "." :relative)
 
-;; (define (find-divisor n test-divisor)
-;;   (cond ((> (square test-divisor) n) n)
-;;         ((divides? test-divisor n) test-divisor)
-;;         (else (find-divisor n (+ test-divisor 1)))))
+(load "ch-1.scm")
+(load "ex-1-22.scm")
+
+; 問題 1.23 以前の定義
+; (define (find-divisor n test-divisor)
+;   (cond ((> (square test-divisor) n) n)
+;         ((divides? test-divisor n) test-divisor)
+;         (else (find-divisor n (+ test-divisor 1)))))
+
+(define (next n)
+  (if (= n 2)
+    3
+    (+ n 2)))
 
 (define (find-divisor n test-divisor)
-  (define (next n)
-    (if (= n 2)
-      3
-      (+ n 2)))
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
         (else (find-divisor n (next test-divisor)))))

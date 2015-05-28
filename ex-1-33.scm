0e89832e2c1a1c703026c86fadb44f23cad8ae34
ocha <ochaochaocha3@gmail.com>
2015-05-28 21:32:00 +0900

    問題 1.33: 解き直し

--- ex-1-33.scm
+++ ex-1-33.scm
@@ -1,21 +1,44 @@
-(load "./ch-1.scm")
+;; Exercise 1.33
 
-(define (filtered-accumulate combiner null-value term filter a next b)
-  (define (next-step)
-    (filtered-accumulate combiner
-                         null-value
-                         term
-                         filter
-                         (next a)
-                         next
-                         b))
-  (cond ((> a b) null-value)
-        ((filter a) (combiner (term a) (next-step)))
-        (else (next-step))))
+(add-load-path "." :relative)
 
-(define (sum-of-squares-of-primes a b)
-  (filtered-accumulate + 0 square prime? a inc b))
+(load "ch-1.scm")
+
+;; 再帰的プロセス
+; (define (filtered-accumulate combiner null-value pred term a next b)
+;   (if (> a b)
+;     null-value
+;     (if (pred a)
+;       (combiner (term a)
+;                 (filtered-accumulate combiner
+;                                      null-value
+;                                      pred
+;                                      term
+;                                      (next a)
+;                                      next
+;                                      b))
+;       (filtered-accumulate combiner
+;                            null-value
+;                            pred
+;                            term
+;                            (next a)
+;                            next
+;                            b))))
+
+;; 反復的プロセス
+(define (filtered-accumulate combiner null-value pred term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a)
+            (if (pred a)
+              (combiner (term a) result)
+              result))))
+  (iter a null-value))
+
+(define (sum-of-squared-primes a b)
+  (filtered-accumulate + 0 prime? square a inc b))
 
 (define (product-of-relatively-prime-numbers n)
-  (define (relatively-prime? i) (= (gcd i n) 1))
-  (filtered-accumulate * 1 identity relatively-prime? 1 inc (- n 1)))
+  (define (relatively-prime? i) (= (gcd n i) 1))
+  (filtered-accumulate * 1 relatively-prime? identity 1 inc (- n 1)))

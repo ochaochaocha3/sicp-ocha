3694a218df573e90738858ea577b0c40c1b10be4
ocha <ochaochaocha3@gmail.com>
2015-05-30 23:44:29 +0900

    問題 1.35: 必要なコードを追加する

--- ex-1-35.scm
+++ ex-1-35.scm
@@ -1,4 +1,14 @@
 ;; Exercise 1.35
-(define golden-ratio
+
+(add-load-path "." :relative)
+
+(load "ch-1.scm")
+
+;; 不動点探索で求めた黄金比
+(define golden-ratio-as-fixed-point
   (fixed-point (lambda (x) (+ 1 (/ 1 x)))
                1.0))
+
+;; 正確な黄金比
+(define accurate-golden-ratio
+  (/ (+ 1 (sqrt 5)) 2.0))
